4b9cee2ea00b227779e03c011c115121e7ac6ea7
Evan Nguyen <evannguyen1101@yahoo.com>
2023-09-19 02:50:35 -0700

    improved performance on scrolling. added some css to handle mobile. fixed zoom breaking the margins by reformatting all css under 'block'

--- next-p5/src/component/scroll/scroll.tsx
+++ next-p5/src/component/scroll/scroll.tsx
@@ -2,45 +2,42 @@ import styles from "./scroll.module.css";
 
 import { useState, useEffect } from 'react';
 
-var limit = 1;
-
 const Scroll = () => {
-    //var limit = Math.max(document.body.scrollHeight, 
-    //    document.body.offsetHeight, 
-    //    document.documentElement.clientHeight, 
-    //    document.documentElement.scrollHeight, 
-    //    document.documentElement.offsetHeight);
-
+    const [limit, setLimit] = useState(1);
     const [scrollY, setScrollY] = useState(0);
 
     useEffect(() => {
-        limit = document.documentElement.clientHeight;
+        const onResize = () => {
+            setLimit(document.documentElement.clientHeight);
+        };
+
+        onResize();
+
+        window.addEventListener("resize", onResize);
+        return () => {
+            window.removeEventListener("resize", onResize);
+        };
+    }, []);
+
+    useEffect(() => {
         const handleScroll = () => {
             setScrollY(window.scrollY);
         };
 
         handleScroll();
-
+        
         window.addEventListener("scroll", handleScroll);
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
     }, []);
 
-    useEffect(() => {
-        function handleResize() {
-          limit = document.documentElement.clientHeight;
-        }
-    
-        window.addEventListener('resize', handleResize)
-    })
-
     return (
         <div className={styles['scroll-bar']}>
            
             <div className={styles['scroll-dot']} style={{ 
                 //transform: `translateY(${(scrollY / limit) * limit * 0.3}px)`
-                top: `calc(${(scrollY / limit) * 100}% - 0.25vw)`
+                top: `calc(${(scrollY / limit) * 100}% - 0.5vw)`
             }}/>
         
         </div>

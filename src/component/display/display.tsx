0635cb0642eb6695bd6eb338fccd404bcb957c21
Evan Nguyen <evannguyen1101@yahoo.com>
2023-10-10 04:42:42 -0700

    working framer motion demo

--- src/component/display/display.tsx
+++ src/component/display/display.tsx
@@ -1,8 +1,11 @@
 import styles from "./display.module.css";
 import cstyles from "@/component/cursor/cursor.module.css";
+
 import { Md, P } from "../interface";
+
 import { useEffect, useState } from "react";
 import { HiOutlineExternalLink } from "react-icons/hi";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Card = (project: Md, setCursor: any) => {
   let date: Date = new Date();
@@ -23,7 +26,7 @@ const Card = (project: Md, setCursor: any) => {
           onMouseEnter={() => setCursor(`${cstyles.onheader}`)}
           onMouseLeave={() => setCursor("")}
         >{project.title} 
-          <div className={styles['icon']}><HiOutlineExternalLink/></div>
+          { project.link ? <div className={styles['icon']}><HiOutlineExternalLink/></div> : <></> }
         </div>
         <div className={styles['description']}>{project.description}</div>
       
@@ -66,13 +69,21 @@ const Display: React.FC<P> = (props) => {
   
   return (
       <div className={styles['column']}>
+        <AnimatePresence>
         { loading ? <Loading/> :
           (projects.map( (slug: any, id: number) => (
-            <div key={id}>
+            <motion.div
+              key={id}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.5, delay: id * 0.1}}
+            >
               {Card(slug, props.setCursor)}
-            </div>
+            </motion.div>
           )))
         } 
+        </AnimatePresence>
       </div>
   )
 }
